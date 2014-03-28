ca08902cdd78d3d5493e93ebf29f89c34f65c018
Matthieu Rossier <matthieu.rossier@he-arc.ch>
2014-03-28 20:00:28 +0100

    [2D Editor] Add ability to create new Connector in diagram

---
+++ src/javascript/Styles.js
@@ -0,0 +1,10 @@
+
+// define the style of Item and Connector
+
+var Styles = {
+	Radius: 0,
+	Color: new draw2d.util.Color(0, 0, 0),
+	BackgroundColor: new draw2d.util.Color(255, 255, 255),
+	Stroke: 1,
+	Alpha: 100
+}
